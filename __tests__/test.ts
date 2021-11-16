767b19d2d29a18d3e56ad91db0c04820cbc2691c
gabojaime <gjaime906@gmail.com>
2021-11-16 18:20:35 -0400

    feat: calculate eiq for activity achievement lot

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -1,4 +1,67 @@
-import { Multiply } from '../src/index';
+import { Multiply, CalculateEiqInList } from '../src/index';
 test('Multiply', () => {
   expect(Multiply(2, 2)).toBe(4);
+});
+
+describe('calculate eiq on activity', () => {
+  test('ActivityEiq', () => {
+    const data = [
+      {
+        total: 100,
+        realizedSurface: 200,
+        eiq: 3.83425,
+      },
+      {
+        total: 50,
+        realizedSurface: 200,
+        eiq: 1.58267,
+      },
+    ];
+
+    const result = CalculateEiqInList(data, 3);
+
+    expect(result).toBe(2.313);
+  });
+});
+
+describe('calculate eiq on achievement', () => {
+  test('AchievementEiq', () => {
+    const data = [
+      {
+        total: 135,
+        realizedSurface: 100,
+        eiq: 3.834,
+      },
+      {
+        total: 75,
+        realizedSurface: 100,
+        eiq: 1.583,
+      },
+    ];
+
+    const result = CalculateEiqInList(data, 2);
+
+    expect(result).toBe(6.36);
+  });
+});
+
+describe('calculate eiq on lot', () => {
+  test('LotEiq', () => {
+    const data = [
+      {
+        total: 135,
+        realizedSurface: 200,
+        eiq: 3.834,
+      },
+      {
+        total: 75,
+        realizedSurface: 200,
+        eiq: 1.583,
+      },
+    ];
+
+    const result = CalculateEiqInList(data, 2);
+
+    expect(result).toBe(3.18);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,30 @@
  * @returns
  */
 export const Multiply = (a: number, b: number) => a * b;
+
+/**
+ * @function Multiply
+ * @description function that receives two parameters as an argument and returns its product
+ * @param a factor a
+ * @param b factor b
+ * @returns
+ */
+
+interface IDataDTO {
+  surface?: number;
+  realizedSurface: number;
+  total: number;
+  eiq: number;
+}
+
+const SumEiq = (current: number, value: number): number => current + value;
+export const CalculateQuantity = (total: number, realizedSurface: number): number => total / realizedSurface;
+
+export const CalculateEiq = (data: IDataDTO) => {
+  const { eiq, total, realizedSurface } = data;
+  const quantity = CalculateQuantity(total, realizedSurface);
+  return quantity * eiq;
+};
+
+export const CalculateEiqInList = (data: IDataDTO[], fixedData: number): number =>
+  parseFloat(data.map(CalculateEiq).reduce(SumEiq).toFixed(fixedData));
