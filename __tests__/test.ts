7c560572cda250789190aed16a848430b70d23fa
gabojaime <gjaime906@gmail.com>
2021-11-16 22:06:27 -0400

    feat: refactoring test and readme

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -1,67 +1,112 @@
-import { Multiply, CalculateEiqInList } from '../src/index';
+import { Multiply, CalculateEiq, CalculateEiqInList } from '../src/index';
 test('Multiply', () => {
   expect(Multiply(2, 2)).toBe(4);
 });
 
 describe('calculate eiq on activity', () => {
-  test('ActivityEiq', () => {
-    const data = [
-      {
-        total: 100,
-        plannedArea: 200,
-        eiq: 3.83425,
-      },
-      {
-        total: 50,
-        plannedArea: 200,
-        eiq: 1.58267,
-      },
-    ];
+  const data = [
+    {
+      total: 100,
+      plannedArea: 200,
+      eiq: 3.83425,
+    },
+    {
+      total: 50,
+      plannedArea: 200,
+      eiq: 1.58267,
+    },
+  ];
 
-    const result = CalculateEiqInList(data, 3);
+  describe('Calculate the eiq of data 1 in the activity', () => {
+    it('should return as result 1.917', () => {
+      const eiq: number = Number(CalculateEiq(data[0]).toFixed(3));
+      expect(eiq).toEqual(1.917);
+    });
+  });
 
-    expect(result).toBe(2.313);
+  describe('Calculate the eiq of data 2 in the activity', () => {
+    it('should return as result 0.396', () => {
+      const eiq: number = Number(CalculateEiq(data[1]).toFixed(3));
+      expect(eiq).toEqual(0.396);
+    });
+  });
+
+  describe('Calculate the eiq of complete activity', () => {
+    it('should return as result 2.313', () => {
+      const eiq: number = Number(CalculateEiqInList(data, 3));
+      expect(eiq).toEqual(2.313);
+    });
   });
 });
 
 describe('calculate eiq on achievement', () => {
-  test('AchievementEiq', () => {
-    const data = [
-      {
-        total: 135,
-        plannedArea: 100,
-        eiq: 3.834,
-      },
-      {
-        total: 75,
-        plannedArea: 100,
-        eiq: 1.583,
-      },
-    ];
+  const data = [
+    {
+      total: 135,
+      plannedArea: 100,
+      eiq: 3.834,
+    },
+    {
+      total: 75,
+      plannedArea: 100,
+      eiq: 1.583,
+    },
+  ];
 
-    const result = CalculateEiqInList(data, 2);
+  describe('Calculate the eiq of data 1 in the achievement', () => {
+    it('should return as result 5.18', () => {
+      const eiq: number = Number(CalculateEiq(data[0]).toFixed(2));
+      expect(eiq).toEqual(5.18);
+    });
+  });
 
-    expect(result).toBe(6.36);
+  describe('Calculate the eiq of data 2 in the achievement', () => {
+    it('should return as result 1.19', () => {
+      const eiq: number = Number(CalculateEiq(data[1]).toFixed(2));
+      expect(eiq).toEqual(1.19);
+    });
+  });
+
+  describe('Calculate the eiq of complete achievemet', () => {
+    it('should return as result 6.36', () => {
+      const eiq: number = Number(CalculateEiqInList(data, 2));
+      expect(eiq).toEqual(6.36);
+    });
   });
 });
 
 describe('calculate eiq on lot', () => {
-  test('LotEiq', () => {
-    const data = [
-      {
-        total: 135,
-        plannedArea: 200,
-        eiq: 3.834,
-      },
-      {
-        total: 75,
-        plannedArea: 200,
-        eiq: 1.583,
-      },
-    ];
+  const data = [
+    {
+      total: 135,
+      plannedArea: 200,
+      eiq: 3.834,
+    },
+    {
+      total: 75,
+      plannedArea: 200,
+      eiq: 1.583,
+    },
+  ];
 
-    const result = CalculateEiqInList(data, 2);
+  describe('Calculate the eiq of data 1 in the lot', () => {
+    it('should return as result 2.59', () => {
+      const eiq: number = Number(CalculateEiq(data[0]).toFixed(2));
+      expect(eiq).toEqual(2.59);
+    });
+  });
 
-    expect(result).toBe(3.18);
+  describe('Calculate the eiq of data 2 in the lot', () => {
+    it('should return as result 0.59', () => {
+      const eiq: number = Number(CalculateEiq(data[1]).toFixed(2));
+      expect(eiq).toEqual(0.59);
+    });
+  });
+
+  describe('Calculate the eiq of complete lot', () => {
+    it('should return as result 3.18', () => {
+      const eiq: number = Number(CalculateEiqInList(data, 2));
+      expect(eiq).toEqual(3.18);
+    });
   });
 });
